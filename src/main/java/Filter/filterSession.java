269ce42399a87ef89ab8c42f15fb258a03eb81de
stouf <ouadad.mustapha01@gmail.com>
2025-03-13 15:31:54 +0000

    creer filter member and coach

---
+++ src/main/java/Filter/filterSession.java
@@ -0,0 +1,53 @@
+package Filter;
+
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+@WebFilter({"/coachDashboard.jsp" , "/memberDashboard.jsp"})
+public class filterSession extends HttpFilter implements Filter {
+
+ 
+
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpSession session = req.getSession(false);
+		if(session == null) {
+			resp.sendRedirect("index.jsp?error=session");
+			return;
+		}
+		
+		Object userId = session.getAttribute("idUser");
+		Object role = session.getAttribute("role");
+		
+		if(userId == null) {
+			resp.sendRedirect("index.jsp?error=session");
+			return;
+		}
+		
+		chain.doFilter(req, resp);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
