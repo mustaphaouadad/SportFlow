a4587674377b349450eed352efd22ef3c0a65a9b
stouf <ouadad.mustapha01@gmail.com>
2025-03-14 10:55:52 +0000

    Ajouter methode getSeancesByCoach dao

--- src/main/java/dao/Userdao.java
+++ src/main/java/dao/Userdao.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Coach;
 import model.Member;
+import model.Seance;
 import model.User;
 
 public class Userdao {
@@ -96,6 +99,27 @@ public class Userdao {
 		  
 		  return user;
 	  }
+	  
+	  
+	  public List<Seance> getSeancesByCoach(int idCoach) {
+	        List<Seance> seances = new ArrayList<>();
+	        String sql = "SELECT s.dateSeance, s.heure, u.fullName AS memberName FROM SeanceDentraînement s JOIN Users u ON s.idMem = u.idUser WHERE s.idCoach = ?";
+	        try {
+	            PreparedStatement pst = coon.prepareStatement(sql);
+	            pst.setInt(1, idCoach);
+	            ResultSet rs = pst.executeQuery();
+	            while (rs.next()) {
+	                Seance s = new Seance();
+	                s.setDateSeance(rs.getString("dateSeance"));
+	                s.setHeure(rs.getString("heure"));
+	                s.setMemberName(rs.getString("memberName"));
+	                seances.add(s);
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return seances;
+	    }
 
 	
 	
