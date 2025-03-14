b52be0d203a8eba66cb8ac1b31753799ac11f985
stouf <ouadad.mustapha01@gmail.com>
2025-03-14 10:53:41 +0000

    modifier f class model seance

---
+++ src/main/java/model/Seance.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Seance {
+	
+	    private String dateSeance;
+	    private String heure;
+	    private String memberName;
+
+	    public String getDateSeance() {
+	        return dateSeance;
+	    }
+
+	    public void setDateSeance(String dateSeance) {
+	        this.dateSeance = dateSeance;
+	    }
+
+	    public String getHeure() {
+	        return heure;
+	    }
+
+	    public void setHeure(String heure) {
+	        this.heure = heure;
+	    }
+
+	    public String getMemberName() {
+	        return memberName;
+	    }
+
+	    public void setMemberName(String memberName) {
+	        this.memberName = memberName;
+	    }
+
+}
