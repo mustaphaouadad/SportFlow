900a8d4f626419625095bc39f7cc97e148225241
stouf <ouadad.mustapha01@gmail.com>
2025-03-14 11:02:09 +0000

    Ajouter servlate SeanceServlet

---
+++ src/main/java/servlet/SeanceServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SeanceServlet
+ */
+@WebServlet("/SeanceServlet")
+public class SeanceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
